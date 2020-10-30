8218bce6eac0ed2dc466a96e59f2b9926bb70c99
InyaevaSvetlana <avgustaella@gmail.com>
2020-10-30 17:53:20 +0300

    lesson1

---
+++ script.js
@@ -0,0 +1,11 @@
+//Задание 1
+const Tc = prompt ("Введите температуру в градусах по Цельсию:");
+const Tf = (9 / 5) * Tc + 32;
+alert ("Ваша температура по Фаренгейту: " + Tf);
+
+//Задание 2
+
+let admin;
+let name = "Василий";
+admin=name;
+console.log (admin);
