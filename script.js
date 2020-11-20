ffed0b06e41de4fc727865b64a77091dac5222bc
InyaevaSvetlana <avgustaella@gmail.com>
2020-11-20 16:55:50 +0300

    js

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
 //Задание 1
-function chessboard() {
-    let newTable = document.createElement( 'table' ),
-        lets = [ '','A','B','C','D','E','F','G','H','' ],
-        blackFigs1 = [ '8','♜','♞','♝','♛','♚','♝','♞','♜','8' ],
-        whiteFigs1 = [ '1','♖','♘','♗','♕','♔','♗','♘','♖','1' ],
-        blackFigs2 = [ '7','♟','♟','♟','♟','♟','♟','♟','♟','7' ],
-        whiteFigs2 = [ '2','♙','♙','♙','♙','♙','♙','♙','♙','2' ];
-    for ( let i = 0, a = 9; i < 10, a >= 0; i++, a-- ) {
-        let newTr = newTable.insertRow(i);
-        for ( let j = 0; j < 10; j++ ) {
-            let newTd = newTr.insertCell( j );
-                        switch (i) {
-                case 0:
-                    newTd.innerText = lets[ j ];
-                    break;
-                case 1:
-                    newTd.innerHTML = blackFigs1[ j ];
-                    break;
-                case 2:
-                    newTd.innerHTML = blackFigs2[ j ];
-                    break;
-                case 7:
-                    newTd.innerHTML = whiteFigs2[ j ];
-                    break;
-                case 8:
-                    newTd.innerHTML = whiteFigs1[ j ];
-                    break;
-                case 9:
-                    newTd.innerText = lets[ j ];
-                    break;
-                default:
-                    if ( j === 0 || j === 9 ) {
-                        newTd.innerHTML = a;
-                    }
-                    break;
-            }
-        }
+const handleClick = (event) => {
+    const src = event.target.getAttribute('src');
+    const oldImg = document.querySelector('.full-picture-item');
+
+    if (oldImg && oldImg.getAttribute('src') === src) {
+        console.log("Stopped");
+        return;
     }
-    document.body.appendChild( newTable );
+    
+    const wrapper = document.querySelector('.full-picture');
+    
+    const img = document.createElement('img');
+    img.classList.add("full-picture-item");
+    img.setAttribute('src', src);
+    if (oldImg) {
+        wrapper.replaceChild(img, oldImg);
+        console.log("Pucture changed");
+    } else {
+        wrapper.appendChild(img);
+        console.log("Pucture added");
+    }
+}
+
+const handleMouseEnter = (event) => {
+    console.log("Mouse entered!");
+    event.target.classList.add('active')
+}
+
+const handleMouseLeave = (event) => {
+    console.log("Mouse left!");
+    event.target.classList.remove('active')
+}
+
+window.onload = () => {
+        const previews = document.querySelectorAll('img.preview-item');
+    for (const item of previews) {
+        item.addEventListener('click', handleClick);
+        item.addEventListener('mouseenter', handleMouseEnter);
+        item.addEventListener('mouseleave', handleMouseLeave);
+    }
+    const shop = document.querySelectorAll ('div.items');
+    for (const food of shop) {
+        food.addEventListener('click', addItem);
+    }
+}
+
+//Задание 2
+
+let items = document.querySelectorAll('items');
+let recycle = document.getElementsByClassName('recycle');
+let infotext = document.getElementsByClassName('basket-info');
+let summ = document.getElementsByClassName('summ')
+
+    function addItem() {
+        let item_clone = items.cloneNode(true);
+        recycle.appendChild(item_clone);
+
+       let prices = recycle.getElementsByClassName('price')
+       totalprice = 0;
+        for (let j = 0; j<prices.length;j++){
+            totalprice = totalprice + Number(prices[j].innerHTML);}
+            console.log('Стоимость всего', totalprice);
+            summ[0].innerHTML = 'Стоимость всего'+ totalprice;
+        }
+        let item_button = document.getElementsByClassName('items-button');
+for (var i = 0; i < item_button.length; i++) {
+    item_button[i].onclick = addItem;
 };
-chessboard();
+
